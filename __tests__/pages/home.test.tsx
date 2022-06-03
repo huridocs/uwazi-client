5158c2fa17e5b0efb33075a44470130cb4d41437
Alberto Casado Torres <konzz@users.noreply.github.com>
2022-06-03 14:15:34 +0200

    CI config for the project (#8)
    * CI config for the project
    
    * setup file for jest tests

--- __tests__/pages/home.test.tsx
+++ __tests__/pages/home.test.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from '../../pages/index';
-import '@testing-library/jest-dom';
 
 describe('Home', () => {
   it('renders a heading', () => {
