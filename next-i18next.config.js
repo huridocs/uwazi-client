3f8cda7bc80b7b77c7163fd1a0e3db2828d7f87a
Mercy <mercy@huridocs.org>
2022-06-06 12:07:54 -0500

    attempt with next-i18n-next

---
+++ next-i18next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    i18n: {
+      defaultLocale: 'es',
+      defaultNS: [],
+      fallbackNS: false,
+      locales: ['en', 'es'],
+      partialBundledLanguages: true,
+      backend: {
+        loadPath: 'http://localhost:3000/api/translations',
+    },
+    },
+  };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { appWithTranslation } from 'next-i18next';
 
 const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
-export default MyApp;
+export default appWithTranslation(MyApp);
