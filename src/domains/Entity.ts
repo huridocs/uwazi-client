8542afc2bb314073c613b4f4dda6dc68e03da742
Santiago <borrajosantiago@gmail.com>
2022-06-22 13:51:58 -0300

    first attempts

--- src/domains/Entity.ts
+++ src/domains/Entity.ts
@@ -2,3 +2,40 @@ export interface EntityPlaceholder {
   _id: string;
   title: string;
 }
+export type PropertyValue = {
+  value?: string | number;
+  key?: string;
+};
+
+export interface Entity {
+  _id: string;
+  sharedId: string;
+  title: string;
+  creationDate: number;
+  template: {
+    _id: string;
+    color: string;
+    name: string;
+  };
+  metadata: {
+    [propertyName: string]: {
+      name: string;
+      type: 'text' | 'numeric' | 'select' | 'date';
+      showInCards: boolean;
+      values: PropertyValue[];
+    };
+  };
+}
+
+export type ProcessedEntity = {
+  title: string;
+  template: {
+    _id: string;
+    color: string;
+    name: string;
+  };
+  metadata: {
+    name: string;
+    value: string | number;
+  }[];
+};
--- src/modules/card/Card.tsx
+++ src/modules/card/Card.tsx
@@ -1,14 +1,55 @@
 import React from 'react';
-import { EntityPlaceholder } from 'domains/Entity';
+import { Entity } from 'domains/Entity';
+import { CardView, CardView2, ProcessedEntity } from './Card.view';
 
 type CardProps = {
-  entity: EntityPlaceholder;
+  entity: Entity;
 };
 
-const Card = ({ entity }: CardProps) => (
-  <article>
-    <h2>{entity.title}</h2>
-  </article>
-);
+//we broke eslint
 
-export { Card };
+const Card = ({ entity }: CardProps) => {
+  const processedEntity = {
+    title: entity.title,
+    template: entity.template,
+    metadata: Object.values(entity.metadata).map(property => {
+      return {
+        name: property.name,
+        value: property.values[0],
+        showInCards: property.showInCards,
+        type: property.type,
+      };
+    }),
+  };
+
+  return (
+    <CardView>
+      <Title>{processedEntity.title}</Title>
+      <Metadata>
+        {processedEntity.metadata.map(
+          property => property.showInCards && <MetadataProperty property={property} />
+        )}
+      </Metadata>
+    </CardView>
+  );
+};
+
+const formatCardMetadata = (metadata: Entity['metadata']) => {
+  return Object.values(metadata)
+    .filter(property => property.showInCards && property.values[0].value)
+    .map(property => {
+      return { name: property.name, value: property.values[0].value! };
+    });
+};
+
+const Card2 = ({ entity }: CardProps) => {
+  const processedEntity: ProcessedEntity = {
+    title: entity.title,
+    template: entity.template,
+    metadata: formatCardMetadata(entity.metadata),
+  };
+
+  return <CardView2 entity={processedEntity} />;
+};
+
+export { Card, Card2 };
