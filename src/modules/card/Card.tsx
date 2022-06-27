7c789ac81d26945ff90926627df0a72f727b92f0
RafaPolit <rafaelpolit@gmail.com>
2022-06-27 10:56:03 -0500

    Removed alternate implementation of card

--- src/modules/card/Card.tsx
+++ src/modules/card/Card.tsx
@@ -1,55 +1,24 @@
 import React from 'react';
-import { Entity } from 'domains/Entity';
-import { CardView, CardView2, ProcessedEntity } from './Card.view';
+import { Entity, ProcessedEntity } from 'domains/Entity';
+import { CardView } from './Card.view';
 
 type CardProps = {
   entity: Entity;
 };
 
-//we broke eslint
+const formatMetadata = (metadata: Entity['metadata']) =>
+  Object.values(metadata)
+    .filter(property => property.showInCards && property.values[0].value)
+    .map(property => ({ name: property.name, value: property.values[0].value as string | number }));
 
 const Card = ({ entity }: CardProps) => {
-  const processedEntity = {
-    title: entity.title,
-    template: entity.template,
-    metadata: Object.values(entity.metadata).map(property => {
-      return {
-        name: property.name,
-        value: property.values[0],
-        showInCards: property.showInCards,
-        type: property.type,
-      };
-    }),
-  };
-
-  return (
-    <CardView>
-      <Title>{processedEntity.title}</Title>
-      <Metadata>
-        {processedEntity.metadata.map(
-          property => property.showInCards && <MetadataProperty property={property} />
-        )}
-      </Metadata>
-    </CardView>
-  );
-};
-
-const formatCardMetadata = (metadata: Entity['metadata']) => {
-  return Object.values(metadata)
-    .filter(property => property.showInCards && property.values[0].value)
-    .map(property => {
-      return { name: property.name, value: property.values[0].value! };
-    });
-};
-
-const Card2 = ({ entity }: CardProps) => {
   const processedEntity: ProcessedEntity = {
     title: entity.title,
     template: entity.template,
-    metadata: formatCardMetadata(entity.metadata),
+    metadata: formatMetadata(entity.metadata),
   };
 
-  return <CardView2 entity={processedEntity} />;
+  return <CardView entity={processedEntity} />;
 };
 
-export { Card, Card2 };
+export { Card };
--- src/modules/card/Card.view.tsx
+++ src/modules/card/Card.view.tsx
@@ -1,62 +1,31 @@
+import React from 'react';
 import { ProcessedEntity } from 'domains/Entity';
 
 type CardViewProps = {
-  children: JSX.Element[] | JSX.Element;
-};
-
-const CardView = ({ children }: CardViewProps) => {
-  return (
-    <article>
-      <header>
-        <p>Template name</p>
-      </header>
-      <h2>Entity title</h2>
-      <section>
-        <dl>
-          <dt>Property 1 name:</dt>
-          <dd>Property 1 value</dd>
-          <dt>Property 2 name:</dt>
-          <dd>Property 2 value</dd>
-        </dl>
-      </section>
-      <footer>
-        <span>Tags</span>
-        <span>Action buttons</span>
-      </footer>
-    </article>
-  );
-};
-
-type CardView2Props = {
   entity: ProcessedEntity;
 };
 
-const CardView2 = ({ entity }: CardView2Props) => {
-  return (
-    <article>
-      <header>
-        <p>{entity.template.name}</p>
-      </header>
-      <h2>{entity.title}</h2>
-      <section>
-        <dl>
-          {entity.metadata.map(property => {
-            return (
-              <>
-                <dt>{property.name}:</dt>
-                <dd>{property.value}</dd>
-              </>
-            );
-          })}
-        </dl>
-      </section>
-      <footer>
-        <span>Tags</span>
-        <span>Action buttons</span>
-      </footer>
-    </article>
-  );
-};
+const CardView = ({ entity }: CardViewProps) => (
+  <article>
+    <header>
+      <p>{entity.template.name}</p>
+    </header>
+    <h2>{entity.title}</h2>
+    <section>
+      <dl>
+        {entity.metadata.map(property => (
+          <>
+            <dt>{property.name}:</dt>
+            <dd>{property.value}</dd>
+          </>
+        ))}
+      </dl>
+    </section>
+    <footer>
+      <span>Tags</span>
+      <span>Action buttons</span>
+    </footer>
+  </article>
+);
 
-export type { ProcessedEntity };
-export { CardView, CardView2 };
+export { CardView };
