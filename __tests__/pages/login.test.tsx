9310e656bd16350b887c8f68109df6890a1589de
Mercy <mercy@huridocs.org>
2022-07-04 12:18:32 -0500

    mock well defined

--- __tests__/pages/login.test.tsx
+++ __tests__/pages/login.test.tsx
@@ -2,33 +2,22 @@ import React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import Login from 'pages/login';
 
+const mockPush = jest.fn();
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    ...jest.requireActual('next/router'),
+    route: '/login',
+    pathname: '/login',
+    push: mockPush,
+  }),
+}));
+
 describe('login', () => {
   //user should login
   //user should be redirected to home page after a successful login
   //user should receive an error message if login fails
   //user should be able to recover password
   //should allow users to see all the app links
-
-  const mock = jest.fn();
-  const mockReplace = jest.mock('next/router', () => ({
-    useRouter: () => ({
-      ...jest.requireActual('next/router'),
-      route: '/login',
-      pathname: '/login',
-      push: mock,
-    }),
-  }));
-
-  // jest.mock('./someModule', () => ({
-  //     ...jest.requireActual('./someModule'),
-  //     api: () => Promise.resolve('foo'),
-  //   }));
-  // nextRouter.useRouter = jest.fn()
-  // nextRouter.useRouter.mockImplementation(() => ({
-  // route: '/welcome/create-password',
-  // pathname: '/welcome/create-password',
-  // replace: mockReplace,
-  // }))
 
   beforeAll(() => {
     render(<Login />);
@@ -42,7 +31,7 @@ describe('login', () => {
     const loginButton = screen.getByText('Login');
     fireEvent.click(loginButton);
 
-    await waitFor(() => expect(mock).toHaveBeenCalledTimes(1));
-    expect(mock).toHaveBeenCalledWith('/library/cards');
+    await waitFor(() => expect(mockPush).toHaveBeenCalledTimes(1));
+    expect(mockPush).toHaveBeenCalledWith('/library/cards');
   });
 });
